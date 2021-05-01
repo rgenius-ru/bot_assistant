bf10e87128643697f0c64c40248979f370ac090b
rg <rgenius@yandex.ru>
2021-05-01 23:12:16 +0300

    Refactoring

--- interface_discord.py
+++ interface_discord.py
@@ -29,16 +29,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
     if message.content == '99!':
+        brooklyn_99_quotes = [
+            'I\'m the human form of the 💯 emoji.',
+            'Bingpot!',
+            (
+                'Cool. Cool cool cool cool cool cool cool, '
+                'no doubt no doubt no doubt no doubt.'
+            ),
+        ]
         response = random.choice(brooklyn_99_quotes)
         await message.channel.send(response)
     elif message.content == 'raise-exception':
