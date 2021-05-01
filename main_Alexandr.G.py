b33ce27dd1904f54488ae8b7e7249c390f1e04a2
AINZOLLGON <alexeypumpkin2019@gmail.com>
2021-05-01 17:07:27 +0300

    добавил функцию заглушку

--- main_Alexandr.G.py
+++ main_Alexandr.G.py
@@ -79,7 +79,7 @@ def command_exit():
         sys.exit(0)
 
 def zaglushka():
-    print("prosto_nichego")
+    print("prosto_ nichego")
 
 def anime():
     if question == 'аниме' or question == 'а':
