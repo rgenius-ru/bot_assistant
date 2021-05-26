b8e50c1829a4570f9da518c6b3b735cd03f9c3d2
rg <rgenius@yandex.ru>
2021-05-26 18:34:52 +0300

    Add big russian intents dataset

--- interface_discord.py
+++ interface_discord.py
@@ -16,7 +16,7 @@ if not TOKEN:
 client = discord.Client()
 
 
-def is_similar_to(text1, text2):  # Похожая на ...
+def is_similar_to(text1, text2, percent=0.35):  # Похожая на ...
     # Добрый вечер
     # Дбрый вечер
     # Добрый вече
@@ -35,9 +35,9 @@ def is_similar_to(text1, text2):  # Похожая на ...
     # Изменение в проценнтах = 4/26 (= 0.33) Добрый день
     distance = nltk.edit_distance(text1, text2)
     difference = distance / len(text1)
-    #print(distance, difference)
+    # print(distance, difference)
 
-    if difference < 0.35:
+    if difference < percent:
         return True
     return False
 
@@ -72,13 +72,13 @@ async def on_message(message):
 
 def clear_phrases(replica):
     replica = replica.lower()
-    alfabet_russ = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-    alfabet_engl = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_russ = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    alphabet_eng = 'abcdefghijklmnopqrstuvwxyz'
     some_symbols = ' -'
 
     replica_copy = ''
     for symbol in replica:
-        if symbol in (alfabet_russ + alfabet_engl + some_symbols):
+        if symbol in (alphabet_russ + alphabet_eng + some_symbols):
             replica_copy += symbol  # replica_copy = replica_copy + symbol
 
     replica = replica_copy
@@ -86,26 +86,14 @@ def clear_phrases(replica):
 
 
 def get_intent(replica):
-    # for items in BOT_CONFIG.items():
-    #     print(items)
-
-    # print(replica)
-
-    for example in hello_examples:
-        if is_similar_to(replica, example.lower()):
-            return 'hello'
-
-    for example in goodbye_examples:
-        if is_similar_to(replica, example.lower()):
-            return 'goodbye'
-
-    for example in music_examples:
-        if is_similar_to(replica, example.lower()):
-            return 'music'
-
-    for example in what_gift_do_you_want_examples:
-        if is_similar_to(replica, example.lower()):
-            return 'what_gift_do_you_want'
+    intent = None
+    for string in intents_list:
+        if string.startswith('##'):
+            index_separator = string.index(':')
+            intent = string[index_separator + 1:]
+            # print(intent)
+        if is_similar_to(replica, string.lower()):
+            return intent
 
     return None
 
@@ -126,28 +114,10 @@ def failure_phrases():
 
 
 def get_answer_from_intent(intent):
-    _hello_responses = ['здоров', 'барев зес', 'здоров', 'Дадова', 'НУ ЗДАРОВА', 'хэлоу май фрэндс', 'здравствуйте']
-    _goodbye_responses = ['goodbye', 'изыди', 'до свидания']
-    _music_responses = ['Мммм, я не разбираюсь в музыке.', 'У меня нет доступа к наушникам.']
-    _what_gift_do_you_want_responses = [
-        'совободные 30 гигабайт',
-        'клюшку для гольфа',
-        'диск со старыми играми',
-        'самосознание', 'душу'
-    ]
+    response = []
 
-    if intent == 'hello':
-        response = _hello_responses
-    elif intent == 'goodbye':
-        response = _goodbye_responses
-    elif intent == 'music':
-        response = _music_responses
-    elif intent == 'what_gift_do_you_want':
-        response = _what_gift_do_you_want_responses
-    else:
-        return None
-
-    answer = random.choice(response)
+    # answer = random.choice(response)
+    answer = 'Насколько я понял, твоё намерение: ' + intent
     return answer
 
 
@@ -175,14 +145,14 @@ def start_dialogue(replica):
         answer = get_answer_from_intent(intent)
 
     #  Если не удалось сгенерировать ответ, то выдать фразу: "Я непонял"; "Перефразируй" и т.п.
-    if not intent:
+    if not answer:
         answer = failure_phrases()
 
     return answer
 
 
-def load_intents():  # Загрузить намерения из файла в память
-    file = open('Intents/intents.txt', 'r')  # Отрытие файла в режиме чтения
+def load_intents_old():  # Загрузить намерения из файла в память
+    file = open('Intents/intents_old.txt', 'r')  # Отрытие файла в режиме чтения
     text = file.read()  # Чтение данных из файла
     file.close()  # Закрытие файла
 
@@ -223,43 +193,24 @@ def load_intents():  # Загрузить намерения из файла в 
         print(intent)
 
 
-BOT_CONFIG = {
-    'intents': {
-        'hello': {
-            'examples': [
-                'qq', 'hi', 'привет', 'welcome to the club body', 'boy next door'
-            ],
-            'responses': [
-                'здоров', 'барев зес', 'здоров', 'Дадова', 'НУ ЗДАРОВА', 'хэлоу май фрэндс', 'здравствуйте'
-            ]
-        },
-        'goodbye': {
-            'examples': [
-                'бб', 'gg', 'прощай дружок', 'goodbye'
-            ],
-            'responses': [
-                'goodbye', 'изыди', 'до свидания'
-            ]
-        }
-    },
-    'failure_phrases': [
-        "Я непонял",
-        "Перефразируй"
-    ]
-}
+def load_intents():  # Загрузить намерения из файла в память
+    file = open('Intents/intents.txt', 'r')  # Отрытие файла в режиме чтения
+    text = file.read()  # Чтение данных из файла
+    file.close()  # Закрытие файла
 
-hello_examples = ['qq', 'hi', 'привет', 'welcome to the club body', 'boy next door']
-hello_responses = ['здоров', 'барев зес', 'здоров', 'Дадова', 'НУ ЗДАРОВА', 'хэлоу май фрэндс', 'здравствуйте']
+    text = text.split('\n')  # Разделение текста и преобразование его в список строк
 
-goodbye_examples = ['бб', 'gg', 'прощай дружок', 'goodbye', 'пока']
-goodbye_responses = ['goodbye', 'изыди', 'до свидания']
+    return text
 
-music_examples = ['Включи музыку.', 'play song']
-music_responses = ['Мммм, я не разбираюсь в музыке.', 'У меня нет доступа к наушникам.']
 
-what_gift_do_you_want_examples = ['какой подарок ты хочешь', 'что тебе подарить', 'братан тебе Чего-Нибудь подарить', 'что тебе необходимо']
-what_gift_do_you_want_responses = ['совободные 30 гигабайт', 'клюшку для гольфа', 'диск со старыми играми', 'самосознание', 'душу']
+# intents_list = [
+    # ['hello', [], []],
+    # ['goodbye', [], []],
+# ]
 
-load_intents()
+# load_intents_old()
+
+# intents - list strings from raw file intents.txt
+intents_list = load_intents()
 
 client.run(TOKEN)
